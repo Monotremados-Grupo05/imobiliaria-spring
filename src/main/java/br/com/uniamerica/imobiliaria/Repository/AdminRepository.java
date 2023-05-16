3cb53181fc6e2862aee0f52268ceb437292930d3
Bulla <pedro-bulla@hotmail.com>
2023-05-16 20:42:34 -0300

    Terminei o Servise de admin

--- src/main/java/br/com/uniamerica/imobiliaria/Repository/AdminRepository.java
+++ src/main/java/br/com/uniamerica/imobiliaria/Repository/AdminRepository.java
@@ -2,12 +2,13 @@ package br.com.uniamerica.imobiliaria.Repository;
 
 import br.com.uniamerica.imobiliaria.Entity.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
 public interface AdminRepository extends JpaRepository<Admin, Long> {
-
+public void desativar(@Param("idAdmin")Long id);
 
 
 }
--- src/main/java/br/com/uniamerica/imobiliaria/Servise/AdminServise.java
+++ src/main/java/br/com/uniamerica/imobiliaria/Servise/AdminServise.java
@@ -1,24 +1,59 @@
 package br.com.uniamerica.imobiliaria.Servise;
 import br.com.uniamerica.imobiliaria.Entity.Admin;
+import br.com.uniamerica.imobiliaria.Repository.AdminRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
 //import br.com.uniamerica.imobiliaria.repository.AdminRepository;
 @Service
 public class AdminServise {
 
+    public List<Admin> listar(){
+
+        return adminRepository.findAll();
+    }
 
 
-public Admin cadastrar(Admin admin) {
-    return admin;
+    @Autowired
+    private AdminRepository adminRepository;
+
+    @Transactional
+    public Admin cadastrar(Admin admin) {
+        if (admin.getName().trim().isEmpty()) {
+            throw new RuntimeException("Erro: ADMIN NULA");
+
+        } else {
+            return this.adminRepository.save(admin);
+        }
+    }
+
+    @Transactional
+    public void atualizar(Long id,Admin admin){
+        if (id ==  admin.getId()){
+            this.adminRepository.save(admin);
+        }else {
+            throw new RuntimeException();
+    }
+
+    }
+
+
+    @Transactional
+    public void desativar(Long id){
+        var admin = this.adminRepository.findById(id);
+        if (id == admin.get().getId()){
+            this.adminRepository.desativar(id);
+        }
+        else {
+                throw new RuntimeException();
+        }
+    }
+
+
+
 }
-}
 
-   // if (admin.getName().trim().isBlank() || )
-
-
-
-
-   // public  List<Admin> () { return this.AdminRepository.findAll();
 
 
 
